203ddbc2be7aa1ae319ea08756db57e99f8e6d62
Christopher Nugent <awedeven+github@gmail.com>
2018-01-14 11:47:43 -0800

    Removed CLI from nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -29,31 +29,3 @@ def print_board(sol):
         line = ['.'] * len(sol)
         line[pos] = 'Q'
         print(' '.join(line))
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('size', help='Size of board to use', type=int)
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-p", "--pretty",
-                        help="print board in a visual format", action="store_true")
-    group.add_argument('-q', '--quiet', help='do not print solutions', action='store_true')
-    parser.add_argument('-c', '--count', help='display the count with each solution',
-                        action='store_true')
-    args = parser.parse_args()
-    try:
-        count = 0
-        for sol in lazy_n_queens(args.size):
-            count += 1
-            if args.count:
-                print('Solutions so far: {}'.format(count))
-            if not args.quiet:
-                if args.pretty:
-                    print_board(sol)
-                    print()
-                else:
-                    print(sol)
-        print('Total solutions: {}'.format(count))
-    except KeyboardInterrupt:
-        print('\nFound {} solutions before interrupt.'.format(count))
-        print('Goodbye!')
